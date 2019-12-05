2ac05106154ad3e8c49041b1dee6d6497d446e69
rezarhtm <reza.rhtm@gmail.com>
2019-12-05 15:37:49 +0700

    Updates

---
+++ precache-manifest.14ea363df1a1ce90cadb5feec1c59b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571dd394701074e914ae2637bdf8b30a",
+    "url": "/yofoodie.git/index.html"
+  },
+  {
+    "revision": "b2dca427b0fc931487ec",
+    "url": "/yofoodie.git/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "a74d9a0ba8f11d2ba77c",
+    "url": "/yofoodie.git/static/css/main.e243a5f2.chunk.css"
+  },
+  {
+    "revision": "b2dca427b0fc931487ec",
+    "url": "/yofoodie.git/static/js/2.4a80b82b.chunk.js"
+  },
+  {
+    "revision": "a74d9a0ba8f11d2ba77c",
+    "url": "/yofoodie.git/static/js/main.075dbfe9.chunk.js"
+  },
+  {
+    "revision": "3dbb5fafd65dee6c2c76",
+    "url": "/yofoodie.git/static/js/runtime-main.8f1144f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/yofoodie.git/static/media/logo.25bf045c.svg"
+  }
+]);
